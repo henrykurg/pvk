48f63b46ba95cb60d599a954c12b9154da14aafc
henrykurg <kurg.henry@khk.ee>
2020-03-08 18:49:29 +0200

    pvk

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,8 @@
-<?php
-//Load libraries
 
-require_once 'libraries/Core.php';
+<?php
+// config file
+require_once 'config/constants.php';
+// Auto Load libraries
+spl_autoload_register(function ($className){
+    require_once 'libraries/'.$className.'.php';
+});
